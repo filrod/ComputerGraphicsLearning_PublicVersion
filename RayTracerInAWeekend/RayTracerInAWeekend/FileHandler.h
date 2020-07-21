eeebb71c6c07a8a87f720c664066398059b47847
Filipe Rodrigues <filipe.rodrigues@ubisoft.com>
2020-07-21 13:43:11 -0400

    Using Gamma Correction for Accurate Color Intensity
    did not render picture yet

--- RayTracerInAWeekend/RayTracerInAWeekend/FileHandler.h
+++ RayTracerInAWeekend/RayTracerInAWeekend/FileHandler.h
@@ -29,8 +29,18 @@ inline void ClearFile(const std::string& fName)
 	}
 }
 
-inline std::string WriteColor(ColorRGB pixelColor)
+inline std::string WriteColor(ColorRGB pixelColor, int samplesPerPixel)
 {
+    auto r = pixelColor.x();
+    auto g = pixelColor.y();
+    auto b = pixelColor.z();
+
+    // Divide the color by the number of samples and gamma-correct for gamma=2.0.
+    auto scale = 1.0f / samplesPerPixel;
+    r = sqrt(scale * r);
+    g = sqrt(scale * g);
+    b = sqrt(scale * b);
+
     int ir = static_cast<int>(255.99 * clamp(pixelColor.r(), 0.0f, 0.999f));
     int ig = static_cast<int>(255.99 * clamp(pixelColor.g(), 0.0f, 0.999f));
     int ib = static_cast<int>(255.99 * clamp(pixelColor.b(), 0.0f, 0.999f));

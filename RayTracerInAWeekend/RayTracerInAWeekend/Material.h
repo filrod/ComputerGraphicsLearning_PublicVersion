eee6b320a54e4d94395842822e48cb82cf18adf8
Filipe Rodrigues <filipe.rodrigues@ubisoft.com>
2020-07-24 16:17:23 -0400

    Metallic Fuzz implementation

--- RayTracerInAWeekend/RayTracerInAWeekend/Material.h
+++ RayTracerInAWeekend/RayTracerInAWeekend/Material.h
@@ -11,12 +11,30 @@ public:
 
 class Lambertian : public Material
 {
+    enum Render_OPTIONS
+    {
+        RANDOM_UINT_SPHERE_DIFFUSE,
+        LAMBERTIAN_DIFFUSE,
+        PBR_HEMISPHERICAL_SCATTERING
+    };
+
 public:
     ColorRGB albedo;
     Lambertian(const ColorRGB& a) : albedo(a) {}
     virtual bool Scatter(const ray& incidentRay, const hitInfo& info, ColorRGB& attenuation, ray& scatteredRay) const override
     {
-        vec3 scatterDir = info.normal + randUintVec();
+        Render_OPTIONS renderOption = PBR_HEMISPHERICAL_SCATTERING;
+        vec3 scatterDir = info.normal;
+        switch (renderOption) {
+        case RANDOM_UINT_SPHERE_DIFFUSE:
+            scatterDir += randPointInUnitSphere();
+            break;
+        case LAMBERTIAN_DIFFUSE:
+            scatterDir += randUintVec();
+            break;
+        default:
+            scatterDir += randPBRScatter(info.normal);
+        }
         scatteredRay = ray(info.p, scatterDir);
         attenuation = albedo;
         return true;
@@ -27,14 +45,15 @@ class Metal : public Material
 {
 public:
     ColorRGB albedo;
-    Metal(const ColorRGB& a) : albedo(a) {}
+    float fuzz;
+    Metal(const ColorRGB& a, float f) : albedo(a), fuzz(f < 1 ? f : 1) {}
 
     virtual bool Scatter(const ray& incidentRay, const hitInfo& info, ColorRGB& attenuation, ray& scatteredRay) const override
     {
         vec3 reflected = incidentRay.get_direction();
         reflected.normalize();
         reflected = reflect(reflected, info.normal);
-        scatteredRay = ray(info.p, reflected);
+        scatteredRay = ray(info.p, reflected + fuzz*randPointInUnitSphere());
         attenuation = albedo;
         return (dot(scatteredRay.get_direction(), info.normal) > 0);
     }
--- RayTracerInAWeekend/RayTracerInAWeekend/RayTracerInAWeekend.h
+++ RayTracerInAWeekend/RayTracerInAWeekend/RayTracerInAWeekend.h
@@ -18,10 +18,3 @@ struct window
 	float pixelProgressX;
 	float pixelProgressY;
 };
-
-// enum Render_OPTIONS
-// {
-//     RANDOM_UINT_SPHERE_DIFFUSE,
-//     LAMBERTIAN_DIFFUSE,
-//     PBR_HEMISPHERICAL_SCATTERING
-// };
